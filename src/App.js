1649ebae9689d7f6a2833c6338b748146b75f4c7
chrystalme <afam_ifeanyi@live.com>
2022-02-12 11:47:40 +0200

    1 file modified

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import Header from './Header';
 import './style.css';
 
 export default function App() {
   return (
-    <div>
-     
-    </div>
+    <>
+     <Header />
+    </>
   );
 }
